d6d241cd2278fe54e023a848f0f2fb4b0c1fb40a
pillys <pillys@163.com>
2015-03-13 17:52:25 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
-# fs-path
-Useful file utitiles.
+fs-path
+========
+[![Build Status](https://secure.travis-ci.org/pillys/fs-path.png)](http://travis-ci.org/pillys/fs-path)
+
+Useful file utitiles. See [API Documentation](http://pillys.github.com/fs-path/#FsPath) for detailed info.
+
+---
+
+### find(path [,pattern] [,callback])
+
+Recurcively scan files or scan files by regex pattern.
+
+Callback break tow arguments: err, list
+
+```js
+var fsPath = require('fs-path');
+
+fsPath.find('/usr/local', function(err, list){
+  console.log(list.dirs);
+  console.log(list.files);
+});
+```
+
+The value list is an object has two keys:
+
+```js
+{
+  dirs: [
+    '/usr/local/test'
+    '/usr/local/test/abc'
+  ],
+  files: [
+    '/usr/local/aaa.png',
+    '/usr/local/test/bbb.gif',
+    '/usr/local/test/abc/ddd.html'
+  ]
+}
+```
+
+If you set a pattern to filter the files or directories, you can do like this:
+
+```js
+var fsPath = require('fs-path');
+
+fsPath.find('/usr/local', /\.png/, function(err, list){
+  console.log(list.dirs);
+  console.log(list.files);
+});
+```
+It will return the list:
+
+```js
+{
+  dirs: [ ],
+  files: [
+    '/usr/local/aaa.png'
+  ]
+}
+```
+
+### findSync(path[ ,pattern])
+
+Sync version of find(). Throws exception on error.
+
+
+### mkdir(path[, callback])
+
+Recursively make, if the parent path not exists, it'll create the directory automatically.
+
+```js
+var fsPath = require('fs-path');
+
+fsPath.mkdir('/usr/local/test1/test2/test3', function(err){
+  console.log('ok');
+});
+```
+
+### mkdirSync(path[ ,pattern])
+
+Sync version of mkdir(). Throws exception on error.
+
+
+### copy(from, dist[, callback])
+
+Copy a file or directory to other path, if the parent path not exists, it'll create the directory automatically.
+
+```js
+var fsPath = require('fs-path');
+
+fsPath.copy('/usr/local/test1/test2/test3', '/usr/local/aaaa', function(err){
+  console.log('ok');
+});
+```
+
+### copySync(path[ ,pattern])
+
+Sync version of copy(). Throws exception on error.
+
+
+### remove(path[, callback])
+
+Delete a file or directory.
+
+```js
+var fsPath = require('fs-path');
+
+fsPath.remove('/usr/local/test1/test2/test3', function(err){
+  console.log('ok');
+});
+```
+
+### removeSync(path[ ,pattern])
+
+Sync version of remove(). Throws exception on error.
+
+
+### writeFile(path, content[, encoding][, callback])
+
+Write a file a file or directory.
+
+Encoding is optional, default is 'utf-8'.
+
+```js
+var fsPath = require('fs-path');
+
+fsPath.writeFile('/usr/local/1.html', content function(err){
+  console.log('ok');
+});
+```
+
+### writeFileSync(path, content[, encoding])
+
+Sync version of writeFile(). Throws exception on error.
---
+++ index.js
@@ -0,0 +1,193 @@
+"use strict";
+var fs = require('fs');
+var path = require('path');
+var async = require('async');
+var child_process = require('child_process');
+
+var fsPath = {
+  _win32: process.platform === 'win32',
+  init: function () {
+    if (!child_process.execSync) {
+      throw new Error('your node.js version is to low(<0.11.12).')
+    }
+    return this;
+  },
+  mkdir: function (dist, callback) {
+    dist = path.resolve(dist);
+    fs.exists(dist, function (exists) {
+      if (!exists) {
+        fsPath.mkdir(path.dirname(dist), function () {
+          fs.mkdir(dist, function (err) {
+            callback && callback(err);
+          });
+        });
+      } else {
+        callback && callback(null);
+      }
+    });
+  },
+  mkdirSync: function (dist) {
+    dist = path.resolve(dist);
+    if (!fs.existsSync(dist)) {
+      fsPath.mkdirSync(path.dirname(dist));
+      fs.mkdirSync(dist);
+    }
+  },
+  copy: function (from, dist, callback) {
+    var that = this,
+      cmd = '';
+    dist = path.resolve(dist);
+    fs.lstat(from, function (err, stats) {
+      if (err) {
+        callback(err);
+      } else {
+        if (that._win32) {
+          // windows
+          if (stats.isDirectory()) {
+            cmd = 'echo da|xcopy /s /e "' + path.join(from, '*') + '" "' + dist + '"';
+          } else {
+            cmd = 'echo fa|xcopy /s /e "' + from + '" "' + dist + '"';
+          }
+        } else {
+          // linux or mac
+          cmd = 'cp -r "' + from + '" "' + dist + '"';
+        }
+        child_process.exec(cmd, function (error, stdout, stderr) {
+          callback && callback(error);
+        });
+      }
+    });
+  },
+  copySync: function (from, dist) {
+    var cmd = '';
+    var stats = fs.lstatSync(from);
+    dist = path.resolve(dist);
+    if (this._win32) {
+      // windows
+      if (stats.isDirectory()) {
+        cmd = 'echo da|xcopy /s /e "' + path.join(from, '*') + '" "' + dist + '"';
+      } else {
+        cmd = 'echo fa|xcopy /s /e "' + from + '" "' + dist + '"';
+      }
+    } else {
+      // linux or mac
+      cmd = 'cp -r "' + from + '" "' + dist + '"';
+    }
+    child_process.execSync(cmd);
+  },
+  remove: function (from, callback) {
+    var cmd = '';
+    if (this._win32) {
+      // windows
+      cmd = 'rd /s /q "' + from + '"';
+    } else {
+      // linux or mac
+      cmd = 'rm -rf "' + from + '"';
+    }
+    child_process.exec(cmd, function (error, stdout, stderr) {
+      callback && callback(error);
+    });
+  },
+  removeSync: function (from) {
+    var cmd = '';
+    if (this._win32) {
+      // windows
+      cmd = 'rd /s /q "' + from + '"';
+    } else {
+      // linux or mac
+      cmd = 'rm -rf "' + from + '"';
+    }
+    child_process.execSync(cmd);
+  },
+  find: function (from, regexp, callback) {
+    var filelist = {
+      dirs: [],
+      files: []
+    };
+    if (typeof regexp === 'function') {
+      callback = regexp;
+      regexp = null;
+    }
+    fs.readdir(from, function (err, files) {
+      if (err) {
+        callback && callback(err);
+      } else {
+        async.each(files, function (file, callback) {
+          var file = path.join(from, file);
+          fs.lstat(file, function (err, stats) {
+            if (err) {
+              callback(err);
+            } else {
+              if (stats.isDirectory()) {
+                if (typeof regexp !== 'object' || regexp.test && regexp.test(file)) {
+                  filelist.dirs.push(file);
+                  fsPath.find(file, function (err, files) {
+                    if (err) {
+                      callback && callback(err);
+                    } else {
+                      filelist.dirs = filelist.dirs.concat(files.dirs);
+                      filelist.files = filelist.dirs.concat(files.files);
+                      callback && callback(null);
+                    }
+                  });
+                }
+              } else {
+                if (!regexp || regexp.test(file)) {
+                  filelist.files.push(file);
+                }
+                callback && callback(null);
+              }
+            }
+          });
+        }, function (err) {
+          if (err) {
+            callback && callback(err);
+          } else {
+            callback && callback(null, filelist);
+          }
+        });
+      }
+    });
+  },
+  findSync: function (from, regexp) {
+    var filelist = {
+      dirs: [],
+      files: []
+    };
+    fs.readdirSync(from).forEach(function (file) {
+      file = path.join(from, file);
+      var stats = fs.lstatSync(file);
+      if (stats.isDirectory()) {
+        if (typeof regexp !== 'object' || regexp.test && regexp.test(file)) {
+          var files = fsPath.findSync(file);
+          filelist.dirs = filelist.dirs.concat(files.dirs);
+          filelist.files = filelist.files.concat(files.files);
+        }
+      } else {
+        filelist.files.push(file);
+      }
+    });
+    return filelist;
+  },
+  writeFile: function (dist, content, encoding, callback) {
+    dist = path.resolve(dist);
+    if (typeof encoding === 'function') {
+      callback = encoding;
+      encoding = 'utf-8';
+    }
+    fs.writeFile(dist, content, {
+      encoding: encoding
+    }, callback);
+  },
+  writeFileSync: function (dist, content, encoding) {
+    dist = path.resolve(dist);
+    if (typeof encoding === 'function') {
+      callback = encoding;
+      encoding = 'utf-8';
+    }
+    fs.writeFileSync(dist, content, {
+      encoding: encoding
+    });
+  }
+};
+module.exports = fsPath.init();
